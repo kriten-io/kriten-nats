907e7c58f32744d052d562fae2208c232b4e261f
Steve Corp <steve@kubecode.io>
2026-04-02 17:21:15 +0100

    Env vars for results.py

--- results.py
+++ results.py
@@ -1,20 +1,19 @@
 import asyncio
 import json
 from nats.aio.client import Client as NATS
+import os
+import sys
 
 async def message_handler(msg):
-    subject = msg.subject
     data = msg.data.decode()
-    # print(f"Received a message on '{subject}': {data}")
     data_object = json.loads(data)
     print(f"Job id: {data_object.get('id')}")
     print(f"stdout: {data_object.get('stdout')}")
 
 async def run():
     nats = NATS()
-
     # Connect to the NATS server
-    await nats.connect("nats://localhost:4222")
+    await nats.connect(f"nats://{NATS_HOST}:{NATS_PORT}")
     # Subscribe to the results subject
     subject = "kriten.job.results"
     await nats.subscribe(subject, cb=message_handler)
@@ -24,4 +23,14 @@ async def run():
         await asyncio.sleep(1)
 
 if __name__ == "__main__":
+    NATS_HOST = os.getenv("NATS_HOST")
+    NATS_PORT = os.getenv("NATS_PORT")
+
+    if not NATS_HOST:
+        print("Environment variable NATS_HOST not set.")
+    if not NATS_PORT:
+        print("Environment variable NATS_PORT not set.")
+
+    if not NATS_HOST or not NATS_PORT:
+        sys.exit()
     asyncio.run(run())
